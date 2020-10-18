27d211d3df3d63249acae1583df7ed87df9273cb
Dom Rodriguez <shymega@shymega.org.uk>
2020-10-18 19:29:05 +0100

    [chore](rustfmt): Apply suggested rustfmt fixes to code
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/bin/pwbot.rs
+++ src/bin/pwbot.rs
@@ -3,15 +3,11 @@ extern crate serenity;
 
 use serenity::async_trait;
 use serenity::client::{ClientBuilder, Context, EventHandler};
-use serenity::model::channel::Message;
 use serenity::framework::standard::{
-    StandardFramework,
-    CommandResult,
-    macros::{
-        command,
-        group
-    }
+    macros::{command, group},
+    CommandResult, StandardFramework,
 };
+use serenity::model::channel::Message;
 use std::env;
 
 #[group]
@@ -26,17 +22,16 @@ impl EventHandler for Handler {}
 #[tokio::main]
 async fn main() {
     let framework = StandardFramework::new()
-        .configure(|c|
-            c.prefix("~"))
+        .configure(|c| c.prefix("~"))
         .group(&GENERAL_GROUP);
 
     let token = env::var("DISCORD_TOKEN").unwrap();
 
     let mut client = ClientBuilder::new(token)
-            .event_handler(Handler)
-            .framework(framework)
-            .await
-            .expect("Error creating client!");
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client!");
 
     if let Err(e) = client.start().await {
         println!("Err: {:?}", e);
