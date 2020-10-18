53261b867e03005d6d0a50355048de43d1812f44
Dom Rodriguez <shymega@shymega.org.uk>
2020-10-18 19:14:50 +0100

    [initial](codebase): Initial commit for PiWarsBot
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/bin/pwbot.rs
@@ -0,0 +1,51 @@
+extern crate pwbot;
+extern crate serenity;
+
+use serenity::async_trait;
+use serenity::client::{ClientBuilder, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+use std::env;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c|
+            c.prefix("~"))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").unwrap();
+
+    let mut client = ClientBuilder::new(token)
+            .event_handler(Handler)
+            .framework(framework)
+            .await
+            .expect("Error creating client!");
+
+    if let Err(e) = client.start().await {
+        println!("Err: {:?}", e);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Ping pong!").await?;
+
+    Ok(())
+}
---
+++ src/bot/mod.rs
@@ -0,0 +1,3 @@
+pub mod handlers;
+pub mod twitter;
+pub mod utils;
